9bbd75112f6e2aaca11e7d636c11a53b8eabbb4d
Sam Jackson <samuelsjackson7@gmail.com>
2020-08-26 15:46:16 -0400

    moved search filters section to own component. added form for filters and made Filters btn interactive.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,7 @@
 <template>
   <div id="app" class="min-h-screen bg-gray-200 antialiased">
     <SiteHeader />
-    <section class="flex justify-between bg-gray-800 px-4 py-3">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3">
-          <svg
-            class="h-6 w-6 fill-current text-gray-600"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
-            />
-          </svg>
-        </div>
-        <input
-          class="bg-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900 text-white rounded-lg pl-10 pr-4 py-2"
-          placeholder="Search by keywords"
-        />
-      </div>
-      <button
-        class="flex items-center hover:bg-gray-600 focus:outline-none focus:shadow-outline bg-gray-700 rounded-lg shadow pl-3 pr-4"
-      >
-        <svg
-          class="h-6 w-6 fill-current text-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-          />
-        </svg>
-        <span class="ml-1 text-white font-medium">Filters</span>
-      </button>
-    </section>
+    <SearchFilters />
     <main class="px-4 py-6">
       <h3 class="text-gray-900 text-xl">Los Angeles</h3>
       <p class="text-gray-600">Live like the stars in these luxurious Southern California estates.</p>
@@ -120,9 +87,11 @@
 
 <script>
 import SiteHeader from "./components/SiteHeader";
+import SearchFilters from "./components/SearchFilters";
+
 export default {
   name: "App",
-  components: { SiteHeader },
+  components: { SiteHeader, SearchFilters },
 };
 </script>
 
---
+++ tailwind.config.js
@@ -0,0 +1,71 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    customForms: (theme) => ({
+      default: {
+        input: {
+          borderRadius: theme("borderRadius.lg"),
+          backgroundColor: theme("colors.gray.200"),
+          "&:focus": {
+            backgroundColor: theme("colors.white"),
+          },
+        },
+
+        select: {
+          backgroundColor: theme("colors.gray.700"),
+          borderColor: "transparent",
+          borderRadius: theme("borderRadius.lg"),
+          boxShadow: theme("boxShadow.default"),
+          color: theme("colors.white"),
+          lineHeight: theme("lineHeight.snug"),
+          // iconColor: theme('colors.white'),
+
+          "&:focus": {
+            backgroundColor: theme("colors.gray.600"),
+            borderColor: "transparent",
+            boxShadow: "none",
+          },
+        },
+
+        checkbox: {
+          width: theme("spacing.6"),
+          height: theme("spacing.6"),
+          backgroundColor: theme("colors.gray.900"),
+          borderColor: "transparent",
+          color: theme("colors.indigo.500"),
+
+          "&:focus": {
+            backgroundColor: theme("colors.gray.600"),
+            borderColor: "transparent",
+            boxShadow: "none",
+
+            "&:checked": {
+              backgroundColor: theme("colors.indigo.500"),
+            },
+          },
+        },
+
+        radio: {
+          width: theme("spacing.6"),
+          height: theme("spacing.6"),
+          backgroundColor: theme("colors.gray.900"),
+          borderColor: "transparent",
+          color: theme("colors.indigo.500"),
+
+          "&:focus": {
+            backgroundColor: theme("colors.gray.600"),
+            borderColor: "transparent",
+            boxShadow: "none",
+
+            "&:checked": {
+              backgroundColor: theme("colors.indigo.500"),
+            },
+          },
+        },
+      },
+    }),
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/custom-forms")],
+};
