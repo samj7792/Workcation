b9719f05a5db37970ec4da1868f98bf9d7be2c02
Sam Jackson <samuelsjackson7@gmail.com>
2020-09-03 16:39:15 -0400

    Responsive design for SiteHeader on sm screens

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-gray-900">
+  <header class="bg-gray-900 sm:flex sm:items-center sm:justify-between">
     <div class="flex justify-between px-4 py-3">
       <div>
         <svg
@@ -28,7 +28,7 @@
         <button
           @click="toggle"
           type="button"
-          class="px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white"
+          class="px-2 text-gray-500 hover:text-white focus:outline-none focus:text-white sm:hidden"
         >
           <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
@@ -46,31 +46,31 @@
         </button>
       </div>
     </div>
-    <nav v-show="isOpen">
-      <div class="px-2 pt-2 pb-5 border-b border-gray-800">
+    <nav class="sm:flex sm:items-center sm:pr-4" :class="{ 'hidden': !isOpen, 'block': isOpen}">
+      <div class="px-2 pt-2 pb-5 border-b border-gray-800 sm:flex sm:border-b-0 sm:py-0 sm:px-0">
         <a
           href="#"
-          class="block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800"
+          class="block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:text-sm sm:px-2"
         >List your property</a>
         <a
           href="#"
-          class="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800"
+          class="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:mt-0 sm:text-sm sm:px-2 sm:ml-2"
         >Trips</a>
         <a
           href="#"
-          class="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800"
+          class="mt-1 block px-3 py-1 rounded font-semibold text-white hover:bg-gray-800 sm:mt-0 sm:text-sm sm:px-2 sm:ml-2"
         >Messages</a>
       </div>
-      <div class="px-5 py-5">
+      <div class="px-5 py-5 sm:py-0 sm:px-0 sm:ml-4">
         <div class="flex items-center">
           <img
-            class="h-10 w-10 object-cover rounded-full border-2 border-gray-600"
+            class="h-10 w-10 object-cover rounded-full border-2 border-gray-600 sm:h-8 sm:w-8"
             src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
             alt="Isla Schoger Profile Pic"
           />
-          <span class="ml-4 font-semibold text-gray-200">Isla Schoger</span>
+          <span class="ml-4 font-semibold text-gray-200 sm:hidden">Isla Schoger</span>
         </div>
-        <div class="mt-5">
+        <div class="mt-5 sm:hidden">
           <a href="#" class="block text-gray-400 hover:text-white">Account settings</a>
           <a href="#" class="mt-3 block text-gray-400 hover:text-white">Support</a>
           <a href="#" class="mt-3 block text-gray-400 hover:text-white">Sign out</a>
